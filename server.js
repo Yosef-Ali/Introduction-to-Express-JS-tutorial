6e0c3e0e0f1ceee1cc6bde81ec8ae0949e891bd8
apple <apple@apples-MacBook-Pro.local>
2022-06-14 10:02:03 +0300

    chaining

--- server.js
+++ server.js
@@ -27,4 +27,21 @@ app.get(
 	}
 );
 
+const one = (req, res, next) => {
+	console.log('one');
+	next();
+};
+
+const two = (req, res, next) => {
+	console.log('two');
+	next();
+};
+
+const three = (req, res) => {
+	console.log('three');
+	res.send('Finished!');
+};
+
+app.get('/chain(.html)?', [one, two, three]);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
